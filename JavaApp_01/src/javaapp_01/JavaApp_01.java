bcfc9023de8e370b395282c716eb82be749d0dda
kels81 <mamaardilla@hotmail.com>
2022-02-21 17:02:48 -0600

    Commit hello world

---
+++ JavaApp_01/src/javaapp_01/JavaApp_01.java
@@ -0,0 +1,16 @@
+package javaapp_01;
+
+/**
+ *
+ * @author Eduardo Cortes hernandez
+ */
+public class JavaApp_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+    
+}
